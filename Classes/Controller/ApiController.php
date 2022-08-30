37697ec5a247b085cdcaddb62b3fe25af5dbf2a8
ujamii.boerner <lb@ujamii.com>
2022-08-30 11:09:29 +0200

    added properties action to api controller

--- Classes/Controller/ApiController.php
+++ Classes/Controller/ApiController.php
@@ -12,10 +12,12 @@ class ApiController extends ActionController
     protected $defaultViewObjectName = JsonView::class;
 
     /**
-     * @param Node $node pass nodes by passing the path: /sites/ujamii-projectpackage/main/node-w7tgzk3629x1p/node-7iftmxgfebsx5/node-0o8m6864vj54s
+     * Returns the rendered html of node
+     *
+     * @param Node $node pass a node by passing the path: /sites/ujamii-projectpackage/main/node-w7tgzk3629x1p/node-7iftmxgfebsx5/node-0o8m6864vj54s
      * @throws \Exception
      */
-    public function renderAction(Node $node)
+    public function renderAction(Node $node): void
     {
         $fusionView = new FusionView();
         $fusionView->setControllerContext($this->controllerContext);
@@ -25,6 +27,25 @@ class ApiController extends ActionController
         $this->view->assign('value', [
             'node' => $node->findNodePath(),
             'html' => $fusionView->render(),
+        ]);
+    }
+
+    /**
+     * Returns the properties of a node
+     *
+     * @param Node $node pass a node by passing the path: /sites/ujamii-projectpackage/main/node-w7tgzk3629x1p/node-7iftmxgfebsx5/node-0o8m6864vj54s
+     * @throws \Neos\ContentRepository\Exception\NodeException
+     * @throws \Neos\ContentRepository\Exception\NodeTypeNotFoundException
+     * @throws \Neos\Flow\Property\Exception
+     * @throws \Neos\Flow\Security\Exception
+     */
+    public function propertiesAction(Node $node): void
+    {
+        $properties = $node->getProperties(true);
+
+        $this->view->assign('value', [
+            'node' => $node->findNodePath(),
+            'properties' => $properties,
         ]);
     }
 }
